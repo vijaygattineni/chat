3b624c88ce6b439738d8d0332707846807933409
Vijay <vijaygattineni369@gmail.com>
2016-06-24 02:20:06 +0530

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var sass = require('gulp-sass');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
---
+++ www/js/services/SocketService.js
@@ -0,0 +1,12 @@
+(function(){
+	angular.module('starter')
+	.service('SocketService', ['socketFactory', SocketService]);
+
+	function SocketService(socketFactory){
+		return socketFactory({
+			
+			ioSocket: io.connect('http://localhost:3000')
+
+		});
+	}
+})();
